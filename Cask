f3d3aedca1f9c9aa511bf28637c97ebe2f986fa6
Charles Lowell <cowboyd@frontside.io>
2014-08-22 22:46:28 -0500

    initial unit tests

---
+++ Cask
@@ -0,0 +1,9 @@
+(source melpa)
+
+(package "mocha-yasnippets" "0.0.1" "YASnippets for the Mocha JavaScript testing framework")
+
+(depends-on "s" "1.3.0")
+(depends-on "dash" "1.0.3")
+
+(development
+ (depends-on "ert-runner"))
---
+++ test/mocha-yasnippets-test.el
@@ -0,0 +1,7 @@
+(ert-deftest mocha-snippets/available-coffee-mode ()
+  "You should be able to use mocha snippets in coffee-mode"
+  ())
+
+(ert-deftest mocha-snippets/available-in-javascript-mode ()
+  "You should be able to use mocha snippets in js-mode"
+  ())
